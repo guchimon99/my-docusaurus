e2b30d81510add50946bea3c037e8bf02a5f937d
guchimon99 <guchimon99@gmail.com>
2023-01-30 12:34:09 +0900

    [ADD] gtag config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,6 +50,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: "GTM-KP8DF7W",
+          anonymizeIP: true
+        }
       }),
     ]
   ],
